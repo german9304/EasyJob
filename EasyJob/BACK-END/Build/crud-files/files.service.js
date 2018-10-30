5c587b69fcf5136a8fb164d582f243aee971905b
german9304 <31298672+german9304@users.noreply.github.com>
2018-10-30 11:53:09 -0700

    files database testing

--- EasyJob/BACK-END/Build/crud-files/files.service.js
+++ EasyJob/BACK-END/Build/crud-files/files.service.js
@@ -2,10 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var gridFiles_1 = require("../Models/gridFiles");
 var getFiles = function (req, res) {
-    var filename = req.params.filename;
-    var file = gridFiles_1.getCandidateFiles(filename);
-    return file.pipe(res);
-    //return res.json(filename);
+    var files = gridFiles_1.getCandidateFiles();
+    files.then(function (files) { return console.log(files); });
+    return res.json({});
 };
 exports.getFiles = getFiles;
 var uploadFile = function (req, res) {
