85545081927b53268336256185e21f8c188729d8
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-02 09:17:59 -0700

    imported files added delete field added update field

--- EasyJob/src/app/jobseeker-dashboard/services/fields.service.ts
+++ EasyJob/src/app/jobseeker-dashboard/services/fields.service.ts
@@ -6,14 +6,43 @@ import {
 import { Injectable } from "@angular/core";
 import { EXPERIENCE, FIELDS, EDUCATION } from "../../job";
 import { Observable, of, throwError } from "rxjs";
+import { List, Map } from "immutable";
+import { AuthService } from "../../services/auth.service";
 import { tap, catchError } from "rxjs/operators";
+import { Router, Route, ActivatedRoute } from "@angular/router";
 
 @Injectable()
 export class FieldService<T> {
-  constructor(private http: HttpClient) {}
+  EXPERIENCE: List<EXPERIENCE> = List();
+  EDUCATION: List<EDUCATION> = List();
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  getField(url: string): Observable<T> {
-    return this.http.get<T>(url).pipe(
+  createField(url: string): Observable<T> {
+    return this.http.post<T>(url, this.auth.UserHeaders).pipe(
+      tap(data => console.log(`field ${JSON.stringify(data)}`)),
+      catchError(error => {
+        console.log(`the error is ${error}`);
+        return this.handleError(error);
+      })
+    );
+  }
+
+  updateField(url: string): Observable<T> {
+    return this.http.put<T>(url, this.auth.UserHeaders).pipe(
+      tap(data => console.log(`field ${JSON.stringify(data)}`)),
+      catchError(error => {
+        console.log(`the error is ${error}`);
+        return this.handleError(error);
+      })
+    );
+  }
+  deleteField(url: string): Observable<T> {
+    return this.http.delete<T>(url, this.auth.UserHeaders).pipe(
       tap(data => console.log(`field ${JSON.stringify(data)}`)),
       catchError(error => {
         console.log(`the error is ${error}`);
