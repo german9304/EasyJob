ec1cea8aba2357684f8e50447b03a37970a1efee
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-05 19:07:18 -0800

    jobs Schema

--- EasyJob/BACK-END/Back-End/Models/jobs-schema.ts
+++ EasyJob/BACK-END/Back-End/Models/jobs-schema.ts
@@ -18,14 +18,14 @@ const jobsSchema: Schema = new mongoose.Schema(
     location: String,
     description: String,
   },
-  { collection: 'jobs', versionKey: false }
+  { collection: 'jobs', versionKey: false },
 );
 
 const categorySchema: Schema = new mongoose.Schema(
   {
     category: String,
   },
-  { collection: 'categories', versionKey: false }
+  { collection: 'categories', versionKey: false },
 );
 
 const jobsModel = mongoose.model('jobs', jobsSchema);
@@ -69,36 +69,34 @@ const jobSearch = async ({ search: field, location }) => {
   // const res = request(
   //   `https://www.ziprecruiter.com/candidate/search?search=${field}&location=${location}`
   // );
-  try {
-    const $ = await rp(options);
+  const $ = await rp(options);
     // console.log($);
     // console.log(res);
-    const jobslist = [];
-    $('#job_list .job_results article').each(function(i, elem) {
+  const jobslist = [];
+  try {
+    $('#job_list .job_results article').each(function () {
       const jobContent = $(this).children('.job_content');
-      const title = job_content.find('.just_job_title');
-      // console.log(title.text());
-      const companyName = job_content.find('.t_org_link.name');
-      // console.log(companyName.text());
-      const location = job_content.find('.t_location_link.location');
-      // console.log(location.text());
-      const description = job_content.find('.job_snippet a');
-      // console.log(description.text());
+      const title = jobContent.find('.just_job_title');
+        // console.log(title.text());
+      const companyName = jobContent.find('.t_org_link.name');
+        // console.log(companyName.text());
+      const location = jobContent.find('.t_location_link.location');
+        // console.log(location.text());
+      const description = jobContent.find('.job_snippet a');
+        // console.log(description.text());
       const job = {
         title: title.text(),
         company: companyName.text(),
         location: location.text(),
         description: description.text().trim(),
       };
-      // console.log("res: ", job);
+        // console.log("res: ", job);
       jobslist.push(job);
     });
-    return jobslist;
   } catch (error) {
-    console.error(error);
-    // expected output: SyntaxError: unterminated string literal
-    // Note - error messages will vary depending on browser
+
   }
+  return jobslist;
 };
 
 export { jobSearch, createAJob, createAcategory, categoryModel };
