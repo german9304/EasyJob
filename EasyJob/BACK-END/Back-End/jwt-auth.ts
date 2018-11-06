163adf408da902e0b2fa5dcfd891e2dcf7fdb893
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-05 21:28:24 -0800

    jwt auth

--- EasyJob/BACK-END/Back-End/jwt-auth.ts
+++ EasyJob/BACK-END/Back-End/jwt-auth.ts
@@ -1,22 +1,24 @@
-//import * as jwt from "jsonwebtoken";
-import { Strategy, ExtractJwt } from "passport-jwt";
-import { use } from "passport";
-import { JWT_SECRET_KEY } from "./client-auth";
-import { userModel } from "./Models/user-schema";
-import { User } from "./user";
+// import * as jwt from "jsonwebtoken";
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { use } from 'passport';
+import { JWT_SECRET_KEY } from './client-auth';
+import { userModel } from './Models/user-schema';
+import { User } from './user';
+
+function jwtPayloadStrategy(jwtPayload, done) {
+  const { _id, email } = jwtPayload;
+  userModel.findById(_id, (err, user) => {
+    if (err) console.log(err);
+    return user
+      ? done(null, { _id, email })
+      : (console.log('user not found'), done(null, false));
+  });
+}
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: JWT_SECRET_KEY.key
+  secretOrKey: JWT_SECRET_KEY.key,
 };
 use(
-  new Strategy(opts, function(jwt_payload, done) {
-    const { _id, email } = jwt_payload;
-    userModel.findById(_id, (err, user) => {
-      if (err) console.log(err);
-      return user
-        ? done(null, { _id, email })
-        : (console.log("user not fount"), done(null, false));
-    });
-  })
+  new Strategy(opts, jwtPayloadStrategy),
 );
