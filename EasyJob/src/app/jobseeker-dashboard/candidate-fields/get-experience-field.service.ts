faae068dbeebda204523f2a62351baf14c0676d1
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-05 21:42:33 -0800

    get-experience-field

--- EasyJob/src/app/jobseeker-dashboard/candidate-fields/get-experience-field.service.ts
+++ EasyJob/src/app/jobseeker-dashboard/candidate-fields/get-experience-field.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from "@angular/core";
-import { EXPERIENCE, EDUCATION, FIELDS } from "../../job";
-import { Observable } from "rxjs";
-import { CandidateFieldsService } from ".././services/candidate-fields.service";
-import { FieldsService } from ".././services/fields.service";
+import { Injectable } from '@angular/core';
+import { EXPERIENCE, EDUCATION, FIELDS } from '../../job';
+import { Observable } from 'rxjs';
+import { CandidateFieldsService } from '.././services/candidate-fields.service';
+import { FieldsService } from '.././services/fields.service';
 import {
   Router,
   Resolve,
   RouterStateSnapshot,
   ActivatedRouteSnapshot
-} from "@angular/router";
+} from '@angular/router';
 @Injectable()
 export class GetExperienceFieldService implements Resolve<EXPERIENCE> {
   constructor(private fs: FieldsService<EXPERIENCE>) {}
@@ -16,8 +16,8 @@ export class GetExperienceFieldService implements Resolve<EXPERIENCE> {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<EXPERIENCE> {
-    const id: string = route.paramMap.get("id");
-    const url: string = `/api/fields/experience/${id}`;
+    const id: string = route.paramMap.get('id');
+    const url = `/api/fields/experience/${id}`;
     return this.fs.getField(url);
   }
 }
