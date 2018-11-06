8f71ad844765a832b68a237a937630994858ba8e
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-05 22:00:14 -0800

    candidate fields module

--- EasyJob/src/app/jobseeker-dashboard/candidate-fields/candidate-fields.module.ts
+++ EasyJob/src/app/jobseeker-dashboard/candidate-fields/candidate-fields.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterModule, Routes, RouterLinkActive } from "@angular/router";
-import { ReactiveFormsModule } from "@angular/forms";
-import { CandidateFieldsRoutingModule } from "./candidate-fields-routing/candidate-fields-routing.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes, RouterLinkActive } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CandidateFieldsRoutingModule } from './candidate-fields-routing/candidate-fields-routing.module';
 
-import { CandidateProfileComponent } from "./candidate-profile/candidate-profile.component";
-import { CandidateProfileViewComponent } from "./candidate-profile/candidate-profile-view/candidate-profile-view.component";
-import { NewExperienceComponent } from "./experience/new-experience/new-experience.component";
-import { TestComponent } from "./testing/test.component";
-import { TestViewComponent } from "./testing/test-view/test-view.component";
-import { EducationComponent } from "./education/education.component";
-import { EducationViewComponent } from "./education/education-view/education-view.component";
-import { EducationListComponent } from "./education-list/education-list.component";
-import { ExperienceListComponent } from "./experience-list/experience-list.component";
-import { EditExperienceComponent } from "./experience/edit-experience/edit-experience.component";
-import { EditEducationComponent } from "./education/edit-education/edit-education.component";
-import { NewEducationComponent } from "./education/new-education/new-education.component";
+import { CandidateProfileComponent } from './candidate-profile/candidate-profile.component';
+import { CandidateProfileViewComponent } from './candidate-profile/candidate-profile-view/candidate-profile-view.component';
+import { NewExperienceComponent } from './experience/new-experience/new-experience.component';
+import { TestComponent } from './testing/test.component';
+import { TestViewComponent } from './testing/test-view/test-view.component';
+import { EducationComponent } from './education/education.component';
+import { EducationViewComponent } from './education/education-view/education-view.component';
+import { EducationListComponent } from './education-list/education-list.component';
+import { ExperienceListComponent } from './experience-list/experience-list.component';
+import { EditExperienceComponent } from './experience/edit-experience/edit-experience.component';
+import { EditEducationComponent } from './education/edit-education/edit-education.component';
+import { NewEducationComponent } from './education/new-education/new-education.component';
 import { CandidateFilesComponent } from './candidate-files/candidate-files.component';
 import { CandidateResumeComponent } from './candidate-files/candidate-resume/candidate-resume.component';
 
