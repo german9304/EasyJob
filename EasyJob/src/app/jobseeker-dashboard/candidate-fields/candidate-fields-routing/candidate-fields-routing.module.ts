17ea759385bd37f47d4bd0ef04880677500e3130
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-11 23:13:59 -0800

    paths

--- EasyJob/src/app/jobseeker-dashboard/candidate-fields/candidate-fields-routing/candidate-fields-routing.module.ts
+++ EasyJob/src/app/jobseeker-dashboard/candidate-fields/candidate-fields-routing/candidate-fields-routing.module.ts
@@ -6,57 +6,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from '../testing/test.component';
 
 import { DataFieldsService } from '../../services/data-fields.resolver.service';
-import { GetExperienceFieldService } from '../get-experience-field.service';
-import { CandidateResumeComponent} from '../candidate-files/candidate-resume/candidate-resume.component';
-import { CandidateProfileComponent } from '../candidate-profile/candidate-profile.component';
-import { NewExperienceComponent } from '../experience/new-experience/new-experience.component';
-import { NewEducationComponent } from '../education/new-education/new-education.component';
-import { EditExperienceComponent } from '../experience/edit-experience/edit-experience.component';
-import { EditEducationComponent } from '../education/edit-education/edit-education.component';
-import { GetEducationFieldService } from '../get-education-field.service';
+// import { GetExperienceFieldService } from '../get-experience-field.service';
+// import { CandidateResumeComponent} from '../candidate-files/candidate-resume/candidate-resume.component';
+// import { CandidateProfileComponent } from '../candidate-profile/candidate-profile.component';
+// import { NewExperienceComponent } from '../experience/new-experience/new-experience.component';
+// import { NewEducationComponent } from '../education/new-education/new-education.component';
+// import { EditExperienceComponent } from '../experience/edit-experience/edit-experience.component';
+// import { EditEducationComponent } from '../education/edit-education/edit-education.component';
+// import { GetEducationFieldService } from '../get-education-field.service';
 
 const candidateFieldsRoutes: Routes = [
-  {
-    path: '',
-    component: CandidateProfileComponent,
-    resolve: {
-      CandidateFields: DataFieldsService
-    },
-    children: [
-      {
-        path: 'create',
-        children: [
-          {
-            path: 'experience',
-            component: NewExperienceComponent
-          },
-          {
-            path: 'education',
-            component: NewEducationComponent
-          }
-        ]
-      },
-      {
-        path: 'update',
-        children: [
-          {
-            path: 'experience/:id',
-            component: EditExperienceComponent,
-            resolve: { field: GetExperienceFieldService }
-          },
-          {
-            path: 'education/:id',
-            component: EditEducationComponent,
-            resolve: { field: GetEducationFieldService }
-          }
-        ]
-      },
-      {
-        path: 'view/resume',
-        component: CandidateResumeComponent
-      }
-    ]
-  },
   {
     path: '',
     redirectTo: '/',
