735ca19c705d72c2277743ec42c6a8ae652c9abd
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-11 22:38:56 -0800

    moved files

--- EasyJob/src/app/jobseeker-dashboard/candidate-fields/candidate-exp-edu-res-fields/candidate-exp-edu-res-fields.module.ts
+++ EasyJob/src/app/jobseeker-dashboard/candidate-fields/candidate-exp-edu-res-fields/candidate-exp-edu-res-fields.module.ts
@@ -1,6 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+
+import { CandidateProfileComponent } from './candidate-profile/candidate-profile.component';
+import { CandidateProfileViewComponent } from './candidate-profile/candidate-profile-view/candidate-profile-view.component';
+import { EducationComponent } from './education/education.component';
+import { EducationViewComponent } from './education/education-view/education-view.component';
+import { EducationListComponent } from './education-list/education-list.component';
+import { ExperienceListComponent } from './experience-list/experience-list.component';
+import { EditExperienceComponent } from './experience/edit-experience/edit-experience.component';
+import { EditEducationComponent } from './education/edit-education/edit-education.component';
+import { NewEducationComponent } from './education/new-education/new-education.component';
+import { CandidateFilesComponent } from './candidate-files/candidate-files.component';
+import { CandidateResumeComponent } from './candidate-files/candidate-resume/candidate-resume.component';
 @NgModule({
   imports: [
     CommonModule
