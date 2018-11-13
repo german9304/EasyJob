28e96e89694567e4dd0f5a169aab782bfd42e7f7
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-12 23:06:31 -0800

    files

---
+++ EasyJob/Back-End/server/Models/db-connection.ts
@@ -0,0 +1,16 @@
+// import { connect, connection } from "mongoose";
+import * as mongoose from 'mongoose';
+import { DATABASE_URL } from '../client-auth';
+
+mongoose.connect(
+  DATABASE_URL,
+  { useNewUrlParser: true },
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connected mongoose');
+});
+
+export default db;
---
+++ EasyJob/Back-End/server/Models/mock-data.js
@@ -0,0 +1,14 @@
+const JOBFIELDS = [
+  "business",
+  "computer science",
+  "software engineering",
+  "nurse",
+  "software architect",
+  "software tester",
+  "software design",
+  "web development",
+  "web",
+  "engineering",
+  "education",
+  "art"
+];
