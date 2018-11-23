3325942554935529c0773629a08a38ee63dd173e
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-22 21:48:10 -0800

    cand-profile

--- client/src/app/jobseeker-dashboard/candidate-fields/candidate-info/candidate-info.module.ts
+++ client/src/app/jobseeker-dashboard/candidate-fields/candidate-info/candidate-info.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileInfoComponent } from './profile-info/profile-info.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
+import { CandProfileComponent } from './cand-profile/cand-profile.component';
 
 @NgModule({
   imports: [CommonModule],
 
-  declarations: [ProfileInfoComponent, ContactInfoComponent]
+  declarations: [ProfileInfoComponent, ContactInfoComponent, CandProfileComponent]
 })
 export class ContactInfoModule {}
