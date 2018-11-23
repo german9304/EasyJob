77f4bcd89aaaad6b7095406c71d0c2518759630c
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-22 22:03:44 -0800

    refactor files

--- client/src/app/jobseeker-dashboard/candidate-fields/candidate-fields.module.ts
+++ client/src/app/jobseeker-dashboard/candidate-fields/candidate-fields.module.ts
@@ -3,21 +3,22 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, RouterLinkActive } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CandidateFieldsRoutingModule } from './candidate-fields-routing/candidate-fields-routing.module';
+import { CandFieldsExpModule } from './cand-fields-exp/cand-fields-exp.module';
+import { ContactInfoModule } from './candidate-info/candidate-info.module';
+
 import { TestComponent } from './testing/test.component';
 import { TestViewComponent } from './testing/test-view/test-view.component';
-import { CandFieldsExpModule } from './cand-fields-exp/cand-fields-exp.module';
 import { CandidateFieldsComponent } from './candidate-fields/candidate-fields.component';
-
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
     CandidateFieldsRoutingModule,
-    CandFieldsExpModule
+    CandFieldsExpModule,
+    ContactInfoModule
   ],
-  declarations: [
-  CandidateFieldsComponent],
+  declarations: [CandidateFieldsComponent],
   exports: []
 })
 export class CandidateFieldsModule {}
