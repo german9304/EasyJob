7547c3f9c0be28dbe2d88810391f2d259ad1ebb7
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-22 19:07:14 -0800

    renamefolders

--- project/build/EasyJob/src/app/test.directive.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-describe('TestDirective', function () {
-    it('should create an instance', function () {
-        // const directive = new TestDirective('TestDirective');
-        // expect(directive).toBeTruthy();
-    });
-});
