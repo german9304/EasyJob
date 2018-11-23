ac4a1374a658ec389f116d24adeff41ae68f58e6
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-22 18:43:44 -0800

    remove folder

--- EasyJob/src/app/services/style-service.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
-
-@Injectable()
-export class StyleServiceService {
-  searchForm = false;
-  control = false;
-  constructor() {}
-}
