776537000ea0fdc946c1a3ca79e446b6760eedb2
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-22 23:19:09 -0800

    profile-percent component

--- client/src/app/jobseeker-dashboard/candidate-fields/profile-info-extra/profile-info-extra.module.ts
+++ client/src/app/jobseeker-dashboard/candidate-fields/profile-info-extra/profile-info-extra.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CandSkillsComponent } from './cand-skills/cand-skills.component';
+import { ProfilePercentComponent } from './profile-percent/profile-percent.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CandSkillsComponent]
+  declarations: [CandSkillsComponent, ProfilePercentComponent]
 })
 export class ProfileInfoExtraModule { }
