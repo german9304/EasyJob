1985ac1c6f5651f434cae1c8c8f6f514337f52ca
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-30 10:49:40 -0800

    search form

--- client/src/app/employer-dashboard/employer-dashboard.module.ts
+++ client/src/app/employer-dashboard/employer-dashboard.module.ts
@@ -6,10 +6,11 @@ import { DashboardViewComponent } from "./dashboard/dashboard-view/dashboard-vie
 import { EmployerNavbarComponent } from './employer-navbar/employer-navbar.component';
 import { NavBarViewComponent } from './employer-navbar/nav-bar-view/nav-bar-view.component';
 import { ProfileDashboardComponent } from './profile-dashboard/profile-dashboard.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   imports: [CommonModule, EmployerRoutingModule],
-  declarations: [EmployerDashboardComponent, DashboardViewComponent, EmployerNavbarComponent, NavBarViewComponent, ProfileDashboardComponent],
+  declarations: [EmployerDashboardComponent, DashboardViewComponent, EmployerNavbarComponent, NavBarViewComponent, ProfileDashboardComponent, SearchFormComponent],
   exports: [EmployerDashboardComponent]
 })
 export class EmployerDashboardModule {}
