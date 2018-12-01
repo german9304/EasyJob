dca966fa0e7d0455b9ed3ed5d0a7f075be7f0f84
german9304 <31298672+german9304@users.noreply.github.com>
2018-11-30 23:45:26 -0800

    mock data

---
+++ client/src/app/employer-dashboard/mock-data.ts
@@ -0,0 +1,11 @@
+const POSTEDJOBS = [];
+
+interface Postedjobs {
+  title: string;
+  location: string;
+  category: string;
+  company: string;
+  industry: string;
+  type: string;
+  description: string;
+}
