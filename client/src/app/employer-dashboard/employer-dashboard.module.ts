30d7869a173b47820d4109cab1316391e927e08b
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-06 22:30:20 -0800

    fix tslint

--- client/src/app/employer-dashboard/employer-dashboard.module.ts
+++ client/src/app/employer-dashboard/employer-dashboard.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { EmployerDashboardComponent } from "./dashboard/dashboard.component";
-import { EmployerRoutingModule } from "./employer-routing/employer-routing.module";
-import { DashboardViewComponent } from "./dashboard/dashboard-view/dashboard-view.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployerDashboardComponent } from './dashboard/dashboard.component';
+import { EmployerRoutingModule } from './employer-routing/employer-routing.module';
+import { DashboardViewComponent } from './dashboard/dashboard-view/dashboard-view.component';
 import { EmployerNavbarComponent } from './employer-navbar/employer-navbar.component';
 import { NavBarViewComponent } from './employer-navbar/nav-bar-view/nav-bar-view.component';
 import { ProfileDashboardComponent } from './profile-dashboard/profile-dashboard.component';
@@ -17,7 +18,21 @@ import { JobsSectionComponent } from './jobs-section/jobs-section.component';
 
 @NgModule({
   imports: [CommonModule, EmployerRoutingModule],
-  declarations: [EmployerDashboardComponent, DashboardViewComponent, EmployerNavbarComponent, NavBarViewComponent, ProfileDashboardComponent, SearchFormComponent, PostJobComponent, CandidatesComponent, FormRow1Component, FormRow2Component, JobListComponent, CandidateListComponent, JobsSectionComponent],
+  declarations: [
+    EmployerDashboardComponent,
+    DashboardViewComponent,
+    EmployerNavbarComponent,
+    NavBarViewComponent,
+    ProfileDashboardComponent,
+    SearchFormComponent,
+    PostJobComponent,
+    CandidatesComponent,
+    FormRow1Component,
+    FormRow2Component,
+    JobListComponent,
+    CandidateListComponent,
+    JobsSectionComponent
+  ],
   exports: [EmployerDashboardComponent]
 })
 export class EmployerDashboardModule {}
