14f9bef8f2832527f5d8b44ddb1f88ba2b29182d
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-09 15:15:02 -0800

    categories

---
+++ client/src/app/app-dashboard/dashboard/categories/categories.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CATEGORY } from '../../../job';
+
+@Component({
+  selector: 'app-categories',
+  template: `
+    <ul id="categories" [class.active]="categories.length > 0">
+      <li
+        class="item category{{i}}"
+        *ngFor="let category of categories; let i = index"
+      >
+        <span
+          class="link-category"
+          (click)="onCategoryClick(category.category)"
+        >
+          {{ category.category }}</span
+        >
+      </li>
+    </ul>
+  `,
+  styleUrls: ['./categories.component.scss']
+})
+export class CategoriesComponent implements OnInit {
+  @Input()
+  categories: CATEGORY[];
+  @Output()
+  clickCategories = new EventEmitter<string>();
+  constructor() {}
+
+  ngOnInit() {}
+  onCategoryClick(category: string) {
+    this.clickCategories.emit(category);
+  }
+}
