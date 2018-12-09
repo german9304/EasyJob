f04b5bb838d7194fc1cbde6e5c9ccfba82cfa3d0
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-09 14:30:57 -0800

    navbar

--- client/src/app/app-dashboard/dashboard-routing/dashboard-routing.module.ts
+++ client/src/app/app-dashboard/dashboard-routing/dashboard-routing.module.ts
@@ -1,42 +1,42 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { RouterModule, Routes } from "@angular/router";
-import { DashboardComponent } from "../dashboard/dashboard.component";
-import { NavBarComponent } from "../nav-bar/nav-bar.component";
-import { LogInComponent } from "../account/log-in/log-in.component";
-import { CreateAccountComponent } from "../account/create-account/create-account.component";
-import { PostjobComponent } from "../postjob/postjob.component";
-import { DataResolverService } from "../../services/auth-data-resolver.service";
-import { JobSearchComponent } from "../job-search/job-search.component";
-import { JobListDataService } from "../job-list-data.service";
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { LogInComponent } from '../account/log-in/log-in.component';
+import { CreateAccountComponent } from '../account/create-account/create-account.component';
+import { PostjobComponent } from '../postjob/postjob.component';
+import { DataResolverService } from '../../services/auth-data-resolver.service';
+import { JobSearchComponent } from '../job-search/job-search.component';
+import { JobListDataService } from '../job-list-data.service';
 
 const appRoutes: Routes = [
   {
-    path: "",
+    path: '',
     component: NavBarComponent,
     resolve: {
       userData: DataResolverService
     },
     children: [
       {
-        path: "",
+        path: '',
         component: DashboardComponent
       },
       {
-        path: "post/job",
+        path: 'post/job',
         component: PostjobComponent
       },
       {
-        path: "jobs",
+        path: 'jobs',
         component: JobSearchComponent,
         resolve: {
           joblist: JobListDataService
         }
       },
       {
-        path: "account",
-        loadChildren: "../account/account.module#AccountModule"
+        path: 'account',
+        loadChildren: '../account/account.module#AccountModule'
       }
     ]
   }
