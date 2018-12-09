305b660f02a224ce734b12a3cd5970cd072eb6c9
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-09 15:34:18 -0800

    handle search

--- client/src/app/app-dashboard/dashboard/search-form/search-form.component.ts
+++ client/src/app/app-dashboard/dashboard/search-form/search-form.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { CATEGORY } from '../../../job';
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
-
-  constructor() { }
+  @Input() formParent: FormGroup;
+  @Input() categories: CATEGORY[];
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.formParent);
   }
+  handleSearch() {
 
+  }
 }
