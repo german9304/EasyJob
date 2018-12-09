d90d43258f3927a9fad2d9fd3e903242e5a4232d
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-09 09:32:37 -0800

    refactor file

--- client/src/app/jobseeker-dashboard/saved-jobs/saved-jobs.component.ts
+++ client/src/app/jobseeker-dashboard/saved-jobs/saved-jobs.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-saved-jobs',
   templateUrl: './saved-jobs.component.html',
-  styleUrls: ['./saved-jobs.component.css']
+  styleUrls: ['../jobs-style.css', './saved-jobs.component.css']
 })
 export class SavedJobsComponent implements OnInit {
   constructor() {}
