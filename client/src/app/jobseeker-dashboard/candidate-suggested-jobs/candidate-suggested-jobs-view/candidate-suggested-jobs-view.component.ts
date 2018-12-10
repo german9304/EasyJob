58837c627e0cf203afdf8bccf6223f7ee0580d6e
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-09 22:18:14 -0800

    jobs list

--- client/src/app/jobseeker-dashboard/candidate-suggested-jobs/candidate-suggested-jobs-view/candidate-suggested-jobs-view.component.ts
+++ client/src/app/jobseeker-dashboard/candidate-suggested-jobs/candidate-suggested-jobs-view/candidate-suggested-jobs-view.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { JOB } from '../../../job';
 @Component({
   selector: 'app-dashboard-view',
   templateUrl: './candidate-suggested-jobs-view.component.html',
   styleUrls: ['./candidate-suggested-jobs-view.component.css']
 })
 export class DashboardViewComponent implements OnInit {
+  @Input() job: JOB;
+  title: string;
+  location: string;
+  company: string;
+  type: string;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const { title, location, company, type } = this.job;
+    this.title = title;
+    this.location = location;
+    this.company = company;
+    this.type = type;
+    // console.log(
+    //   `title: ${title}, location: ${location}, company: ${company}, type:${type}`
+    // );
+  }
 }
