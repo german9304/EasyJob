8cfe1de8bf887db441e258e92fb5be52e1f456a0
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-09 20:57:49 -0800

    job list

--- client/src/app/jobseeker-dashboard/candidate-suggested-jobs/candidate-suggested-jobs-view/candidate-suggested-jobs-view.component.ts
+++ client/src/app/jobseeker-dashboard/candidate-suggested-jobs/candidate-suggested-jobs-view/candidate-suggested-jobs-view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard-view',
   templateUrl: './candidate-suggested-jobs-view.component.html',
-  styleUrls: ['./candidate-suggested-jobs-view.component.scss']
+  styleUrls: ['./candidate-suggested-jobs-view.component.css']
 })
 export class DashboardViewComponent implements OnInit {
   constructor() {}
