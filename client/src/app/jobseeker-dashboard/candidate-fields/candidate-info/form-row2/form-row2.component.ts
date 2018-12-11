c292edc8229c69dd9b97fd6b395840adadee02c0
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-11 10:18:51 -0800

    form row2

--- client/src/app/jobseeker-dashboard/candidate-fields/candidate-info/form-row2/form-row2.component.ts
+++ client/src/app/jobseeker-dashboard/candidate-fields/candidate-info/form-row2/form-row2.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-form-row2',
   templateUrl: './form-row2.component.html',
-  styleUrls: ['./form-row2.component.css']
+  styleUrls: [
+    '../../shared-profile-fields.component.scss',
+    './form-row2.component.css'
+  ]
 })
 export class FormRow2Component implements OnInit {
+  profileInfo = this.fb.group({
+    education: [''],
+    position: [''],
+    description: ['']
+  });
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
