21c9aa92c0eb49e3cb2f8416fc319c3e53613eb6
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-11 09:35:07 -0800

    form

--- client/src/app/jobseeker-dashboard/candidate-fields/candidate-info/form-row1/form-row1.component.ts
+++ client/src/app/jobseeker-dashboard/candidate-fields/candidate-info/form-row1/form-row1.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-form-row1',
   templateUrl: './form-row1.component.html',
   styleUrls: ['./form-row1.component.css']
 })
 export class FormRow1Component implements OnInit {
+  profileInfo = this.fb.group({
+    email: [''],
+    phone: ['']
+  });
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
