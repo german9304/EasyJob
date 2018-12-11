2527637ecf26a7523988c7273b58a3e349bbafbe
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-11 10:11:36 -0800

    form1

--- client/src/app/jobseeker-dashboard/candidate-fields/candidate-info/candidate-info.module.ts
+++ client/src/app/jobseeker-dashboard/candidate-fields/candidate-info/candidate-info.module.ts
@@ -6,6 +6,8 @@ import { CandProfileComponent } from './cand-profile/cand-profile.component';
 import { FormRow1Component } from './form-row1/form-row1.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProfInfoRoutingModule } from './prof-info-routing/prof-info-routing.module';
+import { FormRow2Component } from './form-row2/form-row2.component';
+import { FormRow3Component } from './form-row3/form-row3.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, ProfInfoRoutingModule],
@@ -13,7 +15,9 @@ import { ProfInfoRoutingModule } from './prof-info-routing/prof-info-routing.mod
     ProfileInfoComponent,
     ContactInfoComponent,
     CandProfileComponent,
-    FormRow1Component
+    FormRow1Component,
+    FormRow2Component,
+    FormRow3Component
   ],
   exports: [CandProfileComponent]
 })
---
+++ client/src/app/jobseeker-dashboard/candidate-fields/candidate-info/form-row3/form-row3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-row3',
+  templateUrl: './form-row3.component.html',
+  styleUrls: ['./form-row3.component.css']
+})
+export class FormRow3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
