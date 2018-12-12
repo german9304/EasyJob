e78565a127731f8dce733686007cce858896bcbb
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-11 23:46:38 -0800

    lists

--- client/src/app/search-result-jobs/job-list/job-list.component.ts
+++ client/src/app/search-result-jobs/job-list/job-list.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { JOB } from '../../job';
+import { JobDataService } from '../../services/job-data.service';
 
 @Component({
   selector: 'app-job-list',
   templateUrl: './job-list.component.html',
-  styleUrls: ['./job-list.component.css']
+  styleUrls: ['./job-list.component.scss']
 })
 export class JobListComponent implements OnInit {
+  @Input()
+  jobs: JOB[];
+  TEST = 'TESTING SPAN';
+  @Output()
+  add: EventEmitter<JOB> = new EventEmitter<JOB>();
 
-  constructor() { }
+  constructor(private js: JobDataService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  getWord(word: string) {
+    this.js.searchWord(word);
   }
-
+  clickAdd(job: JOB) {
+    // console.log(job);
+    this.add.emit(job);
+  }
 }
