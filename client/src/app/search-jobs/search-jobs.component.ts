6d385e6758cab241045c755a4c8d69c0ddc572cf
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-11 21:58:29 -0800

    files

--- client/src/app/search-jobs/search-jobs.component.ts
+++ client/src/app/search-jobs/search-jobs.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { CATEGORY } from '../job';
+import { Router, ActivatedRoute } from '@angular/router';
+import { JobDataService } from '../services/job-data.service';
 @Component({
   selector: 'app-search-jobs',
   templateUrl: './search-jobs.component.html',
@@ -12,7 +14,31 @@ export class SearchJobsComponent implements OnInit {
     location: ['']
   });
   CATEGORIES: CATEGORY[] = [];
-  constructor(private fb: FormBuilder) {}
+  clicked = false;
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private jb: JobDataService
+  ) {}
 
   ngOnInit() {}
+  handleCategory(value: string) {
+    if (!value.trim()) {
+      return;
+    }
+    this.Category.setValue(value);
+    this.clicked = true;
+    this.CATEGORIES = [];
+    // console.log("clicked in get click category: ", this.Category.value);
+  }
+
+  handleSearch() {
+    const { category, location } = this.searchForm.value;
+    // console.log(value);
+    this.router.navigate(['/jobs', { search: category, location }]);
+  }
+
+  get Category() {
+    return this.searchForm.get('category') as FormControl;
+  }
 }
