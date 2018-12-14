8855d0907ff28ccdf2969af065e25845559fdfcd
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-13 22:02:55 -0800

    option container

---
+++ client/src/app/account/create-account/type-option/type-option.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TypeOptionComponent } from './type-option.component';
+
+describe('TypeOptionComponent', () => {
+  let component: TypeOptionComponent;
+  let fixture: ComponentFixture<TypeOptionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TypeOptionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TypeOptionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
