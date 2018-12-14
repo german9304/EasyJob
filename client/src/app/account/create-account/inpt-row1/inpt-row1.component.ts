93bca35203927c1a5cf91f6ed5a58ad55f8a2185
german9304 <31298672+german9304@users.noreply.github.com>
2018-12-13 22:24:28 -0800

    form

--- client/src/app/account/create-account/inpt-row1/inpt-row1.component.ts
+++ client/src/app/account/create-account/inpt-row1/inpt-row1.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-inpt-row1',
   templateUrl: './inpt-row1.component.html',
-  styleUrls: ['./inpt-row1.component.css']
+  styleUrls: ['../../sharedstyle.component.scss', './inpt-row1.component.css']
 })
 export class InptRow1Component implements OnInit {
+  @Input() formParent: FormGroup;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- client/src/app/account/create-account/inpt-row2/inpt-row2.component.ts
+++ client/src/app/account/create-account/inpt-row2/inpt-row2.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-inpt-row2',
   templateUrl: './inpt-row2.component.html',
-  styleUrls: ['./inpt-row2.component.css']
+  styleUrls: ['../../sharedstyle.component.scss', './inpt-row2.component.css']
 })
 export class InptRow2Component implements OnInit {
+  @Input() formParent: FormGroup;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
